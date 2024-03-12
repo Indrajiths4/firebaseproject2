77e70c551f1712fb4ba1100fc20f97637a128df9
Indrajith S Nair <95161630+Indrajiths4@users.noreply.github.com>
2024-03-13 01:41:00 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCjSu5o-9Yn79TMBXhSWAOp8XXykgkEVQ8",
-  authDomain: "spotwatt-32d07.firebaseapp.com",
-  projectId: "spotwatt-32d07",
-  storageBucket: "spotwatt-32d07.appspot.com",
-  messagingSenderId: "9499727677",
-  appId: "1:9499727677:web:8bbbca23d2d870359f5754"
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app);
